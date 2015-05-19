d0cd1e8a3ed81cd0b4f6b3677147b604529d97bc
riccardo.lomazzi <riccardo.lomazzi@outlook.it>
2015-05-19 20:12:09 +0200

    Added jssocial plugin and fontawesome
    Added new plugins

---
+++ WEBSITE/JS/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+     
+    $("#share").jsSocials({
+            showLabel: false,
+            showCount: false,
+            shares: ["email", "facebook", "googleplus"]
+        });
+    
+});
+
+function addSocialIcons(){
+    
+   
+}
