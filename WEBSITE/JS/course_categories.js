428a3b07e01c1d8d0ed7958ad18d69ab81d2be32
riccardo.lomazzi <riccardo.lomazzi@outlook.it>
2015-05-25 22:22:13 +0200

    Forgot to commit js file

--- WEBSITE/JS/course_categories.js
+++ WEBSITE/JS/course_categories.js
@@ -3,23 +3,26 @@ $(document).ready(function(){
     $.ajax({
         method:"POST", //metodo per mandare i dati 
         //dataType:"json", //se non si specifica il datatype con json, il dato in entrata va "parsato" con la funzione JSON.parse
-        crossDomain:true, /*forza a prendere i dati dallo stesso dominio
+        crossDomain:false, /*forza a prendere i dati dallo stesso dominio
         nel caso sia necessario*/
-        url:"getFromDB.php", //path del file php a cui fare richiesta
-        data:{id_course_category:2},
+        url:"../PHP/getFromDB.php", //path del file php a cui fare richiesta
+        data:{
+            query: "SELECT * FROM course_categories;"
+        },
         //SUCCESSO!: la risposta dal server viene scritta qui 
         success: function(response){
             //Carica il contenuto scritto con echo dalla pagina php getFromDB.php
-            var courses=JSON.parse(response); 
-            console.log(courses);
+            var course_categories=JSON.parse(response); 
+            console.log(course_categories);
             var el="";
-            console.log(courses.length);
-            for(var i=0;i<courses.length;i++)
+            console.log(course_categories.length);
+            for(var i=0;i<course_categories.length;i++)
             {
-                
-                el+='<div class="course" id="'+courses[i].id+'"> <h2> '+courses[i].title+' </h2> <p> '+courses[i].description+'</p> </div>';  
+                //insert a div with a span
+                var box_category="<div class='category' id='category"+course_categories[0]+"'> <span> "+course_categories[1]+" </span> </div>";
+                $("#select_category_main_box").after(box_category);
             }
-            $('body').html(el);
+            
         },
         //ERRORE: scrive a console l'errore dato
         error: function(request,error){
