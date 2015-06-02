88419ed5108b25e6e46c28a323708b1a6a8dc36f
Andrea Jegher <andreaj-93@hotmail.it>
2015-06-02 12:40:30 +0200

    Added resize js

---
+++ WEBSITE/JS/Resize.js
@@ -0,0 +1,50 @@
+$(document).ready(dynamicAlign);
+
+$(window).resize(dynamicAlign);
+
+function dynamicAlign()
+{
+    var width = window.innerWidth;
+    
+    $('.resizable').each(function() {
+        dynamicAlignElement(width,$( this ))
+    });
+    
+    $('.resizableContainer').each(function() {
+        dynamicReduceMargin(width,$( this ))
+    });
+}
+function dynamicAlignElement(width,elem)
+{
+    if(width>=767)
+        dynamicAlignBig(width,elem);
+    else
+        dynamicAlignSmall(width,elem);                           
+}
+function dynamicAlignBig(width,elem)
+{
+    elem.width("45%");
+}
+function dynamicAlignSmall(width,elem)
+{
+    elem.width("90%");
+}
+function dynamicReduceMargin(width,elem)
+{
+    if(width>=767)
+        dynamicMarginBig(width,elem);
+    else
+        dynamicMarginSmall(width,elem);
+}
+function dynamicMarginBig(width,elem)
+{
+    elem.css('margin', function (index, curValue) {
+    return "5%";
+    });
+}
+function dynamicMarginSmall(width,elem)
+{
+    elem.css('margin', function (index, curValue) {
+    return "0%";
+    });
+}
