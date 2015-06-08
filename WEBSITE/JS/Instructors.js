3b09e0ac15dc0ece97713fa38db9e98e0b8608ac
Andrea Jegher <andreaj-93@hotmail.it>
2015-06-08 12:23:07 +0200

    added icon

--- WEBSITE/JS/Instructors.js
+++ WEBSITE/JS/Instructors.js
@@ -4,13 +4,13 @@ function ready()
 {
 }
 
-function getNumberOfCategories(){
+function getNumberOfInstructors(){
     var total_number;
      $.ajax({
-        url:getPHPFile('courses'),
+        url:"http://biggymjll.altervista.org/getfromdb_instructors.php",
         cache: false,
         method:"POST", //metodo per ricevere i dati 
-        crossDomain: setBrowserCrossDomain(),
+        //crossDomain: setBrowserCrossDomain(),
         data:{/* query: "SELECT COUNT(*) AS counted FROM instructors;"*/
             query:"getnumberofInstructors="
         },
@@ -28,5 +28,28 @@ function getNumberOfCategories(){
 }
 
 function getInstructorOfTheMonth()
+{
+    var instructorOfTheMonth;
+    $.ajax({
+        url:"http://biggymjll.altervista.org/getfromdb_instructors.php",
+        cache: false,
+        method:"POST", //metodo per ricevere i dati 
+        //crossDomain: setBrowserCrossDomain(),
+        data:{/* query: "SELECT instructors.FirstName, instructors.Surname, instructors.Description, instructors.SmallImage, awards.IconUrl FROM instructors JOIN awards ON instructors.PersonID=awards.PersonID WHERE awards.award=".InstructorOfTheMonth.";"*/
+            query:"getnumberofInstructors="
+        },
+        success: function(response){
+           instructorOfTheMonth=(JSON.parse(response));
+           console.log(instructorOfTheMonth);
+        },
+        error: function(request, error) {
+            console.log(error);
+        }
+    }).done(function(){
+         displayInstructorOfTheMonth(instructorOfTheMonth);
+     });  
+}
+
+function displayInstructorOfTheMonth(instructorOfTheMonth)
 {
 }
